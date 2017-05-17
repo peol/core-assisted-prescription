159ee2a38bb3f247fde937b02d7c1a699157ad62
Håkan Axelsson <han@qlik.com>
2017-05-17 09:08:58 +0200

    Adding Redis validation to be a service runing on manager (#7)

--- deploy/validate-swarm-cluster.sh
+++ deploy/validate-swarm-cluster.sh
@@ -22,7 +22,7 @@ fi
 ERRORS=0
 
 # Name of services that should be part of the swarm
-services+=("openresty" "engine" "logstash" "kibana" "viz" "elasticsearch" "mira" "qliktive-qix-session-service")
+services+=("openresty" "engine" "logstash" "kibana" "viz" "elasticsearch" "mira" "qliktive-qix-session-service" "redis")
 elk+=( "elasticsearch" "logstash" "kibana" )
 
 # ----------------------------------
